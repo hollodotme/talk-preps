e99a88864b0192ab3407639e0236d6299b3934f9
Holger Woltersdorf <hw@hollo.me>
2018-04-24 00:11:58 +0200

    Changed logger to queue example

---
+++ agents-of-yield/generator-queue.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\TalkPreps\AgentsOfYield;
+
+use Generator;
+use PDO;
+use const PHP_EOL;
+
+function queue( PDO $pdo ) : Generator
+{
+	$pdo->query( 'CREATE TABLE queue (message TEXT)' );
+	$statement = $pdo->prepare(
+		'INSERT INTO queue (message) VALUES (:message)'
+	);
+	$counter   = 0;
+	while ( true )
+	{
+		$message = yield $counter++;
+		$statement->execute( ['message' => $message] );
+		print 'Sent: ' . $message;
+	}
+}
+
+$queue = queue( new PDO( 'sqlite::memory:' ) );
+$queue->send( 'Foo' );
+print ' (' . $queue->current() . ')' . PHP_EOL;
+$queue->send( 'Bar' );
+print ' (' . $queue->current() . ')' . PHP_EOL;
